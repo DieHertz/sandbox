c9287910f3a601be01a822b790504b21f09e1ec7
DieHertz <diehertz@gmail.com>
2014-10-04 18:14:02 -0400

    add client socket creation

--- main.cpp
+++ main.cpp
@@ -1,37 +1,54 @@
 #include "net/socket.hpp"
 #include <iostream>
 #include <thread>
+#include <chrono>
+
+void client_bot_function(const std::string& address, const std::uint16_t port) {
+    net::socket socket{net::socket_type::client, address, port};
+
+    const auto data = "ping";
+
+    while (const auto bytes_sent = send(socket.get_fd(), data, std::strlen(data), 0)) {
+        if (bytes_sent == -1) throw net::errno_exception{};
+
+        std::this_thread::sleep_for(std::chrono::milliseconds{500});
+    }
+}
+
+void client_handler_function(net::socket socket) {
+    const auto buffer_size = 4096;
+    char buffer[buffer_size];
+
+    while (true) {
+        const auto bytes_received = recv(socket.get_fd(), buffer, buffer_size, 0);
+        if (bytes_received == -1) throw net::errno_exception{};
+
+        const std::string received_string{buffer, buffer + bytes_received};
+        std::cout << "client " << socket.get_fd() << " : " << received_string << std::endl;
+
+        int total_bytes_sent{};
+        while (total_bytes_sent < bytes_received) {
+            const auto bytes_sent = send(socket.get_fd(), buffer + total_bytes_sent, bytes_received - total_bytes_sent, 0);
+            if (bytes_sent == -1) throw net::errno_exception{};
+
+            total_bytes_sent += bytes_sent;
+        }
+
+        if (received_string.find("exit") == 0) throw net::exception{"exit requested"};
+    }
+}
 
 int main(int argc, char** argv) {
     if (argc != 2 && argc != 3) throw std::invalid_argument{"no port and host supplied"};
 
     const std::uint16_t port = std::stoi(argv[1]);
-    const auto address = argc == 3 ? argv[2] : "";
-    net::socket server_socket{address, port};
+    const auto address = argc == 3 ? argv[2] : std::string{};
+    net::socket server_socket{net::socket_type::server, address, port};
+
+    std::thread{client_bot_function, std::cref(address), port}.detach();
 
     while (true) {
         auto client_socket = server_socket.accept();
-
-        std::thread{[] (net::socket client_socket) {
-            try {
-                const auto buffer_size = 4096;
-                char buffer[buffer_size];
-
-                while (true) {
-                    const auto bytes_received = recv(client_socket.get_fd(), buffer, buffer_size, 0);
-                    if (bytes_received == -1) throw net::errno_exception{};
-
-                    int total_bytes_sent{};
-                    while (total_bytes_sent < bytes_received) {
-                        const auto bytes_sent = send(client_socket.get_fd(), buffer + total_bytes_sent, bytes_received - total_bytes_sent, 0);
-                        if (bytes_sent == -1) throw net::errno_exception{};
-
-                        total_bytes_sent += bytes_sent;
-                    }
-                }
-            } catch (const std::exception& e) {
-                std::cerr << e.what() << std::endl;
-            }
-        }, std::move(client_socket)}.detach();
+        std::thread{client_handler_function, std::move(client_socket)}.detach();
     }
 }
--- net/socket.hpp
+++ net/socket.hpp
@@ -7,11 +7,45 @@
 #include <string>
 
 namespace net {
+    enum struct socket_type {
+        client,
+        server
+    };
+
+    inline void connect_socket(const int socket_fd, const addrinfo_ptr_t& info) {
+        const auto connect_result = connect(socket_fd, info->ai_addr, info->ai_addrlen);
+        if (connect_result == -1) throw errno_exception{};
+    }
+
+    inline void bind_listen_socket(const int socket_fd, const addrinfo_ptr_t& info, const int backlog = int{}) {
+        const auto bind_result = bind(socket_fd, info->ai_addr, info->ai_addrlen);
+        if (bind_result == -1) throw errno_exception{};
+
+        const auto listen_result = listen(socket_fd, backlog);
+        if (listen_result == -1) throw errno_exception{};
+    }
+
+    inline int create_socket_fd(const socket_type type, const std::string& address, const std::uint16_t port) {
+        const auto info = get_addrinfo(address, std::to_string(port));
+
+        const auto socket_fd = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+        if (socket_fd == -1) throw errno_exception{};
+
+        switch (type) {
+        case socket_type::client: connect_socket(socket_fd, info); break;
+        case socket_type::server: bind_listen_socket(socket_fd, info); break;
+        default: throw std::invalid_argument{"invalid socket_type"};
+        }
+
+        return socket_fd;
+    }
+
+
     class socket {
     public:
         explicit socket(const int socket_fd) : socket_fd{socket_fd} {}
-        socket(const std::string& address, const std::uint16_t port)
-            : socket_fd{create_socket(address, port)} {
+        socket(const socket_type type, const std::string& address, const std::uint16_t port)
+            : socket_fd{create_socket_fd(type, address, port)} {
         }
 
         socket(socket&& other) : socket_fd{other.socket_fd} { other.socket_fd = 0; }
@@ -37,21 +71,6 @@ namespace net {
         int get_fd() const { return socket_fd; }
 
     private:
-        static int create_socket(const std::string& address, const std::uint16_t port) {
-            const auto info = get_addrinfo(address, std::to_string(port));
-
-            const auto socket_fd = ::socket(info->ai_family, info->ai_socktype, info->ai_protocol);
-            if (socket_fd == -1) throw errno_exception{};
-
-            const auto bind_result = bind(socket_fd, info->ai_addr, info->ai_addrlen);
-            if (bind_result == -1) throw errno_exception{};
-
-            const auto listen_result = listen(socket_fd, 0);
-            if (listen_result == -1) throw errno_exception{};
-
-            return socket_fd;
-        }
-
         int socket_fd{};
     };
 }
